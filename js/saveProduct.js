c6a7bddd0fc4406cc3c81a9bcd362ca4f678d7b1
norberto-jn <norbertojunio@gmail.com>
2022-04-28 19:43:06 -0300

    adicionando campode de endereço

--- js/saveProduct.js
+++ js/saveProduct.js
@@ -7,7 +7,14 @@ function saveProduct() {
         "whatsapp": document.getElementById('whatsapp').value,
         "value": document.getElementById('value').value,
         "categoryCode": Number(document.getElementById('category').value),
-        "image": document.getElementById('image').value
+        "image": document.getElementById('image').value,
+        "address": {
+            "cep": document.getElementById('cep').value,
+            "logradouro": document.getElementById('rua').value,
+            "bairro": document.getElementById('bairro').value,
+            "localidade": document.getElementById('cidade').value,
+            "uf": document.getElementById('uf').value
+        }
     })
 
     const http = new XMLHttpRequest()
@@ -15,12 +22,15 @@ function saveProduct() {
     http.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
             console.log(http.status)
+            if (http.status == 201) {
+                document.getElementById('alert').innerHTML = '<div class="alert alert-success" role="alert">Imovel salvo com sucesso!</div>'
+            } else
+                document.getElementById('alert').innerHTML = '<div class="alert alert-danger" role="alert">Infelizmente não foi possível salvar o imóvel!</div>'
         }
     })
 
     http.open("POST", url)
     http.setRequestHeader("Content-Type", "application/json")
-    console.log(data)
     http.send(data)
 
 
